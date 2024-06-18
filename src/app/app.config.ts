272c136e84a6c441b68b3198517874e1b2616f65
Okan Kaplan <okan149@gmail.com>
2024-06-18 17:28:45 +0200

    adding firebase to project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"danotes-3edc1","appId":"1:65492954698:web:faad3d75ef4dddd86780b3","storageBucket":"danotes-3edc1.appspot.com","apiKey":"AIzaSyDCRkdOT1krE7cL68ZxfZDUUswMWCKAU88","authDomain":"danotes-3edc1.firebaseapp.com","messagingSenderId":"65492954698"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
