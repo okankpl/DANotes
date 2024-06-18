16ee836b661e91f85f9ddda17eba9cb34a2aa716
Okan Kaplan <okan149@gmail.com>
2024-06-18 21:56:40 +0200

    adding imports to note-list-services.ts

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable ,inject} from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import { Firestore, collectionData, collection,doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
+  firestore: Firestore = inject(Firestore);
+
   constructor() { }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getSingleDocRef(collId:string, docId:string) {
+    return doc(collection(this.firestore, collId), docId);
+  }
 }
