44bfd1c2de3079994ae4e2017b46cbfd4f0464c0
Okan Kaplan <okan149@gmail.com>
2024-06-19 21:48:36 +0200

    adding subscribe

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,26 +1,59 @@
-import { Injectable ,inject} from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Note } from '../interfaces/note.interface';
-import { Firestore, collectionData, collection,doc } from '@angular/fire/firestore';
+import { Firestore, collectionData, collection, doc, onSnapshot } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class NoteListService {
+
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
 
   firestore: Firestore = inject(Firestore);
 
-  constructor() { }
+
+  items$: any;
+  items;
+
+  // unSubList:() => void;
+  // unSubSingle:() => void;
+
+  constructor() {
+    // this.unSubList = onSnapshot(this.getNotesRef(), (list) => {
+    //   list.forEach(element => {
+    //     console.log(element);
+    //   })
+    // });
+   
+
+    this.items$ = collectionData(this.getNotesRef());
+    this.items = this.items$.subscribe((list: []) => {
+      if (list) {
+         list.forEach((element: any) => {
+        console.log(element);
+      });
+      }
+     
+    })    
+  }
+
+
+  ngOnDestroy() {
+
+  }
 
   getTrashRef() {
-    return collection(this.firestore, 'trash');
+    return collection(this.firestore, 'Trash');
   }
 
   getNotesRef() {
-    return collection(this.firestore, 'notes');
+    return collection(this.firestore, 'Notes');
   }
 
-  getSingleDocRef(collId:string, docId:string) {
+  getSingleDocRef(collId: string, docId: string) {
     return doc(collection(this.firestore, collId), docId);
   }
 }
